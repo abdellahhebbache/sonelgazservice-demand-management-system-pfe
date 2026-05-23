5789b52bf2b61620d13e474e93597d5631fa7a47
abdellahhebbache <abdellahhebbache7@gmail.com>
2026-05-23 04:32:03 +0200

    Update propositions.component.ts

--- src/app/pages/propositions/propositions.component.ts
+++ src/app/pages/propositions/propositions.component.ts
@@ -31,14 +31,14 @@ export class PropositionsComponent implements OnInit {
   constructor(private propositionService: PropositionService, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any[]>('http://localhost:8082/Api/metier').subscribe(data => {
+    this.http.get<any[]>('https://sonelgazservice-demand-management-system-pfe-production.up.railway.app/Api/metier').subscribe(data => {
       this.metiers = data;
     });
   }
 
   onMetierChange(): void {
     if (this.selectedIdMetier) {
-      this.http.get<any[]>(`http://localhost:8082/Api/departements/par-metier/${this.selectedIdMetier}`)
+      this.http.get<any[]>(`https://sonelgazservice-demand-management-system-pfe-production.up.railway.app/Api/departements/par-metier/${this.selectedIdMetier}`)
         .subscribe(data => {
           this.departements = data;
           this.proposition.departement = null;
