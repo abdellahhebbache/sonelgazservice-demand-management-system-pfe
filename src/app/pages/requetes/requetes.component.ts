057b30294da18c84f48e38415d2258c15dde8d8f
abdellahhebbache <abdellahhebbache7@gmail.com>
2026-05-23 04:25:44 +0200

    Update requetes.component.ts

--- src/app/pages/requetes/requetes.component.ts
+++ src/app/pages/requetes/requetes.component.ts
@@ -31,14 +31,14 @@ export class RequetesComponent implements OnInit {
   constructor(private requeteService: RequeteService, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any[]>('http://localhost:8080/Api/metier').subscribe(data => {
+    this.http.get<any[]>('https://sonelgazservice-demand-management-system-pfe-production.up.railway.app/Api/metier').subscribe(data => {
       this.metiers = data;
     });
   }
 
   onMetierChange(): void {
     if (this.selectedIdMetier) {
-      this.http.get<any[]>(`http://localhost:8080/Api/departements/par-metier/${this.selectedIdMetier}`)
+      this.http.get<any[]>(`https://sonelgazservice-demand-management-system-pfe-production.up.railway.app/Api/departements/par-metier/${this.selectedIdMetier}`)
         .subscribe(data => {
           this.departements = data;
           this.requete.departement = null;
